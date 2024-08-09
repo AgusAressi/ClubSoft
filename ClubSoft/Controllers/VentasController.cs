64403f1aefa9580386eb1cba238beab4ff5ba383
jorgeghella <jorgeghella09@gmail.com>
2024-08-08 21:17:33 -0300

    vista modal ventas

---
+++ ClubSoft/Controllers/VentasController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ClubSoft.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ClubSoft.Data;
+using static ClubSoft.Models.Venta;
+
+namespace ClubSoft.Controllers;
+
+public class VentasController : Controller
+{
+    private ApplicationDbContext _context;
+
+    public VentasController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+           return View();
+    }
+      }
