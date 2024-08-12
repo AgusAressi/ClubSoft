f9f3acd7873fd3480e463a326f998f873876af1b
Agustin Aressi <agus_aressi@hotmail.com>
2024-08-12 19:35:12 -0300

    cambios sidebar

--- ClubSoft/wwwroot/js/site.js
+++ ClubSoft/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownTriggers = document.querySelectorAll('[data-bs-toggle="collapse"]');
+
+    dropdownTriggers.forEach(function (trigger) {
+        trigger.addEventListener('click', function (event) {
+            var currentDropdown = document.querySelector(this.getAttribute('href'));
+
+            // Cerrar otros dropdowns
+            var openDropdowns = document.querySelectorAll('.collapse.show');
+            openDropdowns.forEach(function (dropdown) {
+                if (dropdown !== currentDropdown) {
+                    var collapseInstance = bootstrap.Collapse.getInstance(dropdown);
+                    collapseInstance.hide();
+                }
+            });
+        });
+    });
+});
