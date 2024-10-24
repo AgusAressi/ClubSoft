b1f3ca30e2ae96b8eabf853208df1802357fe5e2
jorgeghella <jorgeghella09@gmail.com>
2024-10-23 21:52:36 -0300

    graficos version 2

--- ClubSoft/wwwroot/js/Graficos.js
+++ ClubSoft/wwwroot/js/Graficos.js
@@ -59,3 +59,35 @@ function renderizarGrafico() {
 $(document).ready(function () {
     renderizarGrafico();
 });
+
+$(document).ready(function() {
+    // Llamar al método ContarSocios al cargar la página
+    $.ajax({
+        url: '/Graficos/ContarSocios', // Cambia "TuNombreControlador" por el nombre correcto
+        type: 'GET',
+        success: function(data) {
+            $('#totalSocios').text(data.cantidadSocios); // Actualizar el texto de la card
+        },
+        error: function(xhr, status, error) {
+            console.error("Error al obtener la cantidad de socios:", error);
+            $('#totalSocios').text("Error");
+        }
+    });
+});
+
+$(document).ready(function() {
+    // Llamar al método SumarTotalesVentas al cargar la página
+    $.ajax({
+        url: '/Graficos/SumarTotalesVentas', // Cambia "Graficos" por el nombre de tu controlador si es diferente
+        type: 'GET',
+        success: function(data) {
+            // Actualizar el texto de la card con el total de ventas formateado con el signo de peso
+            $('#totalVentas').text('$ ' + data.totalVentas.toFixed(2)); // Asegúrate de tener un ID totalVentas en tu card
+        },
+        error: function(xhr, status, error) {
+            console.error("Error al obtener el total de ventas:", error);
+            $('#totalVentas').text("Error"); // Mostrar error en la card
+        }
+    });
+});
+
