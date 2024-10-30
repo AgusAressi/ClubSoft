2bbaf1472c88e7bc43335c6ae3a0d30d2c74f69a
Agustin Aressi <agus_aressi@hotmail.com>
2024-10-30 18:08:01 -0300

    vista informes

---
+++ ClubSoft/Controllers/InformesController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ClubSoft.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ClubSoft.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClubSoft.Controllers;
+
+public class InformesController : Controller
+{
+    private  ApplicationDbContext _context;
+
+    public InformesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+}
